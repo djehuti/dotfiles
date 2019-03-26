16189eeade78e055b711def1235441ca96fb0b7f
Ben Cox <cox@djehuti.com>
2019-03-26 08:31:24 -0400

    More emacs customization

--- elisp/custom.el
+++ elisp/custom.el
@@ -9,7 +9,6 @@
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(column-number-mode t)
- '(line-number-mode nil)
  '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
  '(custom-safe-themes
    (quote
@@ -39,11 +38,51 @@
  '(flycheck-flake8-maximum-line-length 100)
  '(fringe-mode (quote (12 . 12)) nil (fringe))
  '(goto-address-highlight-p t)
+ '(ibuffer-saved-filter-groups nil)
+ '(ibuffer-saved-filters
+   (quote
+    (("Dropbox"
+      (filename . ".*/Dropbox/"))
+     ("programming"
+      (or
+       (derived-mode . prog-mode)
+       (mode . ess-mode)
+       (mode . compilation-mode)))
+     ("text document"
+      (and
+       (derived-mode . text-mode)
+       (not
+        (starred-name))))
+     ("TeX"
+      (or
+       (derived-mode . tex-mode)
+       (mode . latex-mode)
+       (mode . context-mode)
+       (mode . ams-tex-mode)
+       (mode . bibtex-mode)))
+     ("web"
+      (or
+       (derived-mode . sgml-mode)
+       (derived-mode . css-mode)
+       (mode . javascript-mode)
+       (mode . js2-mode)
+       (mode . scss-mode)
+       (derived-mode . haml-mode)
+       (mode . sass-mode)))
+     ("gnus"
+      (or
+       (mode . message-mode)
+       (mode . mail-mode)
+       (mode . gnus-group-mode)
+       (mode . gnus-summary-mode)
+       (mode . gnus-article-mode))))))
  '(icomplete-mode t nil (icomplete))
  '(indicate-buffer-boundaries (quote right))
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
  '(line-move-visual nil)
+ '(line-number-mode nil)
+ '(menu-bar-mode nil)
  '(minibuffer-max-depth nil)
  '(mouse-avoidance-mode (quote jump) nil (avoid))
  '(mouse-wheel-mode t)
@@ -52,13 +91,19 @@
  '(ns-antialias-text t)
  '(ns-command-modifier (quote meta))
  '(ns-right-command-modifier (quote super))
- '(org-startup-indented t)
+ '(org-confirm-babel-evaluate nil)
+ '(org-hide-emphasis-markers t)
+ '(org-special-ctrl-a/e t)
  '(org-src-fontify-natively t)
  '(org-src-tab-acts-natively t)
- '(org-hide-emphasis-markers t)
+ '(org-startup-indented t)
+ '(package-archives
+   (quote
+    (("melpa-stable" . "http://stable.melpa.org/packages/")
+     ("melpa" . "http://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (csound-mode slime go-mode bazel-mode json-mode markdown-mode ## yaml-mode rtags projectile php-mode magit flycheck flx-ido editorconfig company color-theme-sanityinc-solarized cmake-mode use-package)))
+    (org-bullets which-key csound-mode slime go-mode bazel-mode json-mode markdown-mode ## yaml-mode rtags projectile php-mode magit flycheck flx-ido editorconfig company color-theme-sanityinc-solarized cmake-mode use-package)))
  '(partial-completion-mode nil)
  '(rst-indent-comment 4)
  '(rst-indent-field 4)
@@ -72,7 +117,6 @@
  '(scroll-bar-mode nil)
  '(scroll-step 1)
  '(tool-bar-mode nil)
- '(menu-bar-mode nil)
  '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -80,12 +124,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Fira Code" :foundry "outline" :slant normal :weight normal :height 102 :width normal))))
- '(fixed-pitch ((t (:family "Fira Code" :foundry "outline" :slant normal :weight normal :height 102 :width normal))))
- '(variable-pitch ((t (:family "Sans Serif" :slant normal :weight light :height 102 :width normal))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
- '(org-block ((t (:inherit fixed-pitch :background "#002028" :foreground unspecified))))
- '(italic ((t (:family "Source Code Variable" :foundry "outline" :slant oblique :weight normal :height 102 :width normal))))
  '(cursor ((t (:background "#ff0000"))))
+ '(fixed-pitch ((t (:family "Fira Code" :weight normal :height 102 :width normal))))
  '(font-lock-builtin-face ((t (:foreground "#859900"))))
  '(font-lock-comment-face ((t (:foreground "#93a1a1" :italic t :family "Source Code Variable"))))
  '(font-lock-constant-face ((t (:foreground "#2aa198"))))
@@ -96,16 +136,28 @@
  '(font-lock-type-face ((t (:foreground "#b58900"))))
  '(font-lock-variable-name-face ((t (:foreground "#268bd2"))))
  '(font-lock-warning-face ((t (:foreground "#dc322f" :bold t))))
- '(org-block ((t (:foreground unspecified :background "#002028"))))
  '(hl-line ((t (:background "#073642"))))
+ '(isearch ((t (:background "#859900" :foreground "#fdf6e3"))))
+ '(italic ((t (:family "Source Code Variable" :foundry "outline" :slant oblique :weight normal :height 102 :width normal))))
  '(line-number ((t (:foreground "#586e75"))))
  '(line-number-current-line ((t (:background "#586e75" :foreground "#002b36"))))
- '(isearch ((t (:background "#859900" :foreground "#fdf6e3"))))
  '(mode-line ((t (:background "#93a1a1" :foreground "#002b36"))))
- '(mode-line-inactive ((t (:background "#93a1a1" :foreground1 "#93a1a1"))))
  '(mode-line-buffer-id ((t (:inherit mode-line :foreground "#002b36" :weight bold))))
+ '(mode-line-inactive ((t (:background "#93a1a1" :foreground1 "#93a1a1"))))
+ '(org-block ((t (:inherit fixed-pitch :background "#002028" :foreground unspecified))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "SystemWindowText" :font "Source Sans Pro" :height 1.5 :underline nil))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "SystemWindowText" :font "Source Sans Pro" :height 1.3))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "SystemWindowText" :font "Source Sans Pro" :height 1.2))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "SystemWindowText" :font "Source Sans Pro" :height 1.1))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "SystemWindowText" :font "Source Sans Pro"))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "SystemWindowText" :font "Source Sans Pro"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "SystemWindowText" :font "Source Sans Pro"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "SystemWindowText" :font "Source Sans Pro"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "SystemWindowText" :font "Source Sans Pro"))))
  '(region ((t (:background "#ddddcc"))))
  '(trailing-whitespace ((t (:background "#ccd8ff"))))
+ '(variable-pitch ((t (:family "Source Sans Pro" :height 102 :weight light))))
  '(vertical-border ((t (:background "#aaaa99" :foreground "#aaaa99")))))
 
 ;;; custom.el ends here
