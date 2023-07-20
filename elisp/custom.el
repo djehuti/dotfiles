5db70fd1abe290fcd39ad169b216f005b48f9bbb
Ben Cox <cox@djehuti.com>
2023-07-19 23:40:55 -0400

    Use Noto, and fix mosh path.

--- elisp/custom.el
+++ elisp/custom.el
@@ -30,7 +30,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 120))))
+ '(default ((t (:height 120 :family "Noto Mono"))))
  '(cursor ((t (:background "#ff0000")))))
 
 ;;; custom.el ends here
