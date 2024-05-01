5c58a4ea230eefe779ec48b89e8cb05d3666d6c2
Ben Cox <cox@djehuti.com>
2024-05-01 09:37:55 -0400

    I guess emacs likes it this way.

--- elisp/custom.el
+++ elisp/custom.el
@@ -18,41 +18,25 @@
        `(:foreground ,(face-foreground 'eshell-ls-directory)))
       (propertize "$ " 'face
                   `(:foreground ,(face-foreground 'default))))))
- '(eshell-prompt-regexp "^[^#$
-]* ?[#$] ")
+ '(eshell-prompt-regexp "^[^#$\12]* ?[#$] ")
  '(fringe-mode 0 nil (fringe))
  '(icomplete-mode t nil (icomplete))
  '(mouse-avoidance-mode 'jump nil (avoid))
  '(mouse-wheel-mode t)
  '(package-selected-packages
-   '(cmake-mode
-     editorconfig
-     flx-ido
-     go-mode
-     json-mode
-     magit
-     markdown-mode
-     php-mode
-     rust-mode
-     solarized-theme
-     sudo-edit
-     swift-mode
-     terraform-mode
-     typescript-mode
-     use-package
-     yaml-mode))
+   '(haskell-mode cmake-mode editorconfig flx-ido go-mode json-mode magit markdown-mode php-mode rust-mode solarized-theme sudo-edit swift-mode terraform-mode typescript-mode use-package yaml-mode))
  '(partial-completion-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line-buffer-id ((t (:foreground "#fdf6e3" :underline nil))))
- '(mode-line-inactive ((t (:foreground "#b8c0c4" :background "#93a1a1" :box nil :underline nil :overline nil))))
- '(mode-line ((t (:foreground "#fdf6e3" :background "#839496" :box nil :underline nil :overline nil))))
+ '(default ((t (:height 135 :family "Iosevka"))))
+ '(cursor ((t (:background "#ff0000"))))
  '(line-number ((t (:foreground "#fdf6e3" :background "#ddd7c8"))))
  '(line-number-current-line ((t (:foreground "#839496" :background "#eee8d5"))))
- '(default ((t (:height 135 :family "Iosevka"))))
- '(cursor ((t (:background "#ff0000")))))
+ '(mode-line ((t (:foreground "#fdf6e3" :background "#839496" :box nil :underline nil :overline nil))))
+ '(mode-line-buffer-id ((t (:foreground "#fdf6e3" :underline nil))))
+ '(mode-line-inactive ((t (:foreground "#b8c0c4" :background "#93a1a1" :box nil :underline nil :overline nil)))))
 
 ;;; custom.el ends here
